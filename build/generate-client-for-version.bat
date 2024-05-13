28bc20bb5ec3c0977e62f153ffc3d03398136b7f
pa.pecherskij <pa.pecherskij@interfax.ru>
2024-05-13 10:51:25 +0300

    fix readme

--- build/generate-client-for-version.bat
+++ build/generate-client-for-version.bat
@@ -5,7 +5,7 @@ if NOT [%1]==[] goto usage
 set /p "%inputArg%=Enter keycloak version for which you need client: "
 :usage
 
-:: disable getting fresh codegen jar until release with new csharp codegen is released 
+:: disable getting fresh codegen jar until release with new csharp codegen is released (https://github.com/Fildrance/swagger-codegen/tree/feature/issue-%2312366)
 :: curl https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.55/swagger-codegen-cli-3.0.55.jar -o swagger-codegen-cli.jar
 curl https://raw.githubusercontent.com/ccouzens/keycloak-openapi/main/keycloak/%inputArg%.json -o contracts.json
 
